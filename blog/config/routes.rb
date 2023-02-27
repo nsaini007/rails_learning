0c1124b966d34ce5324ae7d6e6bf78d13b1a2b91
Udit <uthakkar@gammastack.com>
2023-02-27 11:44:03 +0530

    Initial code

---
+++ blog/config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  get 'my/index'
+  # get 'main/index'
+  root "articles#index"
+
+  get "/main", to: "main#index"
+  get "/articles", to: "articles#index"
+  get "/articles/:id", to: "articles#show"
+end
---
+++ blog/db/migrate/20230224065729_create_articles.rb
@@ -0,0 +1,22 @@
+# class CreateArticles < ActiveRecord::Migration[7.0]
+#   def change
+#     create_table :articles do |t|
+#       t.string :title
+#       t.string :string
+#       t.string :body
+#       t.string :text
+
+#       t.timestamps
+#     end
+#   end
+# end
+class CreateArticles < ActiveRecord::Migration[7.0]
+  def change
+    create_table :articles do |t|
+      t.string :title
+      t.text :body
+
+      t.timestamps
+    end
+  end
+end
